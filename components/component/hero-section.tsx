d0efafbc30c7f43099aacb426376368e2f8e8593
Miracle5222 <roneilgungobbansas5222@gmail.com>
2026-01-02 02:31:50 +0800

    remove prelist button

--- components/component/hero-section.tsx
+++ components/component/hero-section.tsx
@@ -94,12 +94,12 @@ export function HeroSectionOne() {
           className="relative z-10 mt-8 flex flex-wrap items-center justify-center gap-4"
         >
           <Link href="/waitlist">
-            <button className="w-60 flex justify-center transform rounded-lg cursor-pointer bg-black px-6 py-2 font-medium text-white transition-all duration-300 hover:-translate-y-0.5 hover:bg-gray-800 dark:bg-white dark:text-black dark:hover:bg-gray-200">
+            {/* <button className="w-60 flex justify-center transform rounded-lg cursor-pointer bg-black px-6 py-2 font-medium text-white transition-all duration-300 hover:-translate-y-0.5 hover:bg-gray-800 dark:bg-white dark:text-black dark:hover:bg-gray-200">
               Pre-List Now
               <span className="pl-2 "> 
                 <MoveRightIcon />
               </span>
-            </button>
+            </button> */}
           </Link>
         </motion.div>
       </div>
